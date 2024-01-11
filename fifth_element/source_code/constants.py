4eb6aa98ca5ba023135c82f2a0d26d7d384f6473
NastyaZdanovich <7nastya17@mail.ru>
2024-01-11 10:21:14 +0300

    delete fifth element

--- fifth_element/source_code/constants.py
+++
@@ -1,5 +0,0 @@
-class Host:
-    FIFTH_EL = 'https://5element.by/'
-
-class Info:
-    ARTICLE = '150252'
--- fifth_element/source_code/pages/basket_page.py
+++
@@ -1,9 +0,0 @@
-from fifth_element.source_code.pages.main_page import MainPage
-
-
-class BasketPage(MainPage):
-    def __init__(self, driver):
-        super().__init__(driver)
-
-        # .text возвращает название товра в корзине
-        self.item_name_in_a_basket = "//*[@class='c-part']//*[@class='c-text']"
